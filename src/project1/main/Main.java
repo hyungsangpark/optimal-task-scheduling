51018a76759a507837e622ec477042e5d0361ac1
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-07-29 14:13:10 +1200

    Created base file that we'll start implementing on.

---
+++ src/project1/main/Main.java
@@ -0,0 +1,11 @@
+package project1.main;
+
+import project1.util.FileLoader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
