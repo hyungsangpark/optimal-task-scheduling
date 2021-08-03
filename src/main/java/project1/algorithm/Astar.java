91291e380f29106de028c23afb3ae2cf4296bda5
yli667 <69701294+yli667@users.noreply.github.com>
2021-08-04 03:09:27 +1200

    implemented basics of heuristics function in Astar.java and update ScheduleNode.java

--- src/main/java/project1/algorithm/Astar.java
+++ src/main/java/project1/algorithm/Astar.java
@@ -5,32 +5,52 @@ import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 import project1.data.ScheduleNode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Astar {
 
-    public Graph _graph;
+    public Graph _taskGraph;
     public int _processors;
+    public LinkedList<Node> _taskNodeList = new LinkedList<>();
     public LinkedList<Node> _openList = new LinkedList<>();
     public LinkedList<Node> _closedList = new LinkedList<>();
 
-    public Astar(Graph graph, int processors) {
-        _graph = graph;
+    public LinkedList<ScheduleNode> _tempOpenList = new LinkedList<>();
+    public LinkedList<ScheduleNode> _tempClosedList = new LinkedList<>();
+
+    public Astar(Graph taskGraph, int processors) {
+        _taskGraph = taskGraph;
         _processors = processors;
     }
 
     public void aStarSearch() {
 
+        //Initialise taskNodeList
+        for (int i=0; i<_taskGraph.getNodeCount();i++) {
+            _taskNodeList.add(_taskGraph.getNode(i));
+        }
+
+        //Create a graph for schedule nodes, create List to hold the partial schedules
         Graph scheduleGraph = new SingleGraph("schedule");
-        List<List<Character>> schedule = null;
-        ScheduleNode root = new ScheduleNode(schedule, "root");
+        List<List<String>> schedule = new ArrayList<>();
+        for (int i=0; i<_processors; i++) {
+            List<String> oneProcess = new ArrayList<>();
+            schedule.add(oneProcess);
+        }
 
-
+        //Create root node(empty schedule)
+        ScheduleNode root = new ScheduleNode(scheduleGraph, schedule, "root");
+        _tempOpenList.add(root);
+        //Heuristics of the node
+        root.setHeuristics(findHeuristic(root));
+        System.out.println(findHeuristic(root));
 
         Node chosen;
 
-        Node node = _graph.getNode(0);
+        Node node = _taskGraph.getNode(0);
         _closedList.add(node);
         LinkedList<Node> childrenList = getChildren(node);
 
@@ -38,13 +58,13 @@ public class Astar {
         chosen = childrenList.get(0);
         int nodeWeight = (int) chosen.getAttribute("weight");
         String edge = node.toString() + chosen;
-        int totalWeight = nodeWeight + (int) _graph.getEdge(edge).getAttribute("weight");
+        int totalWeight = nodeWeight + (int) _taskGraph.getEdge(edge).getAttribute("weight");
         //}
 
         for(int i=0; i<childrenList.size() - 1; i++) {
             Node next = childrenList.get(i+1);
             String nextEdge = node + next.toString();
-            int nextTotalWeight = (int) node.getAttribute("weight") + (int) _graph.getEdge(nextEdge).getAttribute("weight");
+            int nextTotalWeight = (int) node.getAttribute("weight") + (int) _taskGraph.getEdge(nextEdge).getAttribute("weight");
             if (nextTotalWeight < totalWeight) {
                 chosen = next;
             }
@@ -66,7 +86,29 @@ public class Astar {
         //}
         return children;
     }
-    public int findHeuristic() {
-        return -1;
+
+    public int findHeuristic(ScheduleNode node) {
+        //Find occurrence of unique names to figure out the amount of tasks not scheduled yet
+        List<List<String>> currentSchedule = node.getSchedule();
+        int count=0;
+
+        //Clone taskNodeList
+        LinkedList<Node> taskNodeList = _taskNodeList;
+
+        //Find heuristics
+        for (int i=0; i<_processors;i++) {
+            List<String> oneProcess = currentSchedule.get(i);
+            HashSet<String> uniqueList = new HashSet(oneProcess);
+            taskNodeList.removeAll(uniqueList);
+        }
+
+        for (Node n: taskNodeList) {
+            String s = n.toString();
+            if (!s.equals("-1")) {
+                int temp = (int) _taskGraph.getNode(s).getAttribute("weight");
+                count = count + temp;
+            }
+        }
+        return count;
     }
 }
--- src/main/java/project1/data/ScheduleNode.java
+++ src/main/java/project1/data/ScheduleNode.java
@@ -8,14 +8,15 @@ import java.util.List;
 
 public class ScheduleNode {
     private Node _self;
-    private List<List<Character>> _schedule;
+    private List<List<String>> _schedule;
     private List<ScheduleNode> _children = new ArrayList<>();
     private ScheduleNode _parent;
     private String _name;
     private Graph _graph;
+    private int _heuristics;
 
     //For normal schedule nodes
-    public ScheduleNode(Graph graph, List<List<Character>> schedule, ScheduleNode parent, String name) {
+    public ScheduleNode(Graph graph, List<List<String>> schedule, ScheduleNode parent, String name) {
         _graph = graph;
         _schedule = schedule;
         _parent = parent;
@@ -24,9 +25,11 @@ public class ScheduleNode {
     }
 
     //For root schedule node(node with no tasks assigned)
-    public ScheduleNode(List<List<Character>> schedule, String name) {
+    public ScheduleNode(Graph graph, List<List<String>> schedule, String name) {
+        _graph = graph;
         _schedule = schedule;
         _name = name;
+        _self = graph.addNode(name);
     }
 
     public void addChildren(int numOfChildren) {
@@ -35,11 +38,15 @@ public class ScheduleNode {
         }
     }
 
-    public List<List<Character>> getSchedule() {
+    public List<List<String>> getSchedule() {
         return _schedule;
     }
 
     public String getScheduleNodeName() {
         return _self.toString();
+    }
+
+    public void setHeuristics(int num) {
+        _heuristics = num;
     }
 }
