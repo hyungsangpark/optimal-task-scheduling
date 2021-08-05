cecb1d06c9644c45220adac57b57cdc90f471bfb
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-08-05 17:53:17 +1200

    Modified GraphLoader to manually write graph and be instantiatable.
    It had to be instantiatable because it needed to store a field information called graphID and with it, I thought it would be more logical for it to be instantiatable.

--- src/main/java/project1/util/GraphLoader.java
+++ src/main/java/project1/util/GraphLoader.java
@@ -2,8 +2,6 @@ package project1.util;
 
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
-import org.graphstream.stream.file.FileSink;
-import org.graphstream.stream.file.FileSinkDOT;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceDOT;
 
@@ -14,13 +12,24 @@ import java.io.*;
  */
 public class GraphLoader {
 
+    private String _graphID;
+
     /**
      * Reads a graph from a file with .dot file extension.
      * @param linkToGraphFile A link to the graph file, relative to the parent directory of the source folder.
      * @return A graph with nodes and edges read from the given file.
      * @throws IOException when a file with a given link to graph file cannot be found.
      */
-    public static Graph readGraph(String linkToGraphFile) throws IOException {
+    public Graph readGraph(String linkToGraphFile) throws IOException {
+
+        // Read graph ID manually, due to graphstream not loading graph ID properly.
+        BufferedReader graphFileReader = new BufferedReader(new FileReader(linkToGraphFile));
+        String header = graphFileReader.readLine();
+        // Extract graph ID from header line of .dot file.
+        _graphID = header.substring(9, header.length() - 3);
+        graphFileReader.close();
+
+        // Create graph and load the information from graph file to the graph instance.
         Graph graph = new SingleGraph(linkToGraphFile);
         FileSource fileSource = new FileSourceDOT();
 
@@ -35,12 +44,45 @@ public class GraphLoader {
      * @param graph A graph to write .dot file from.
      * @param outputFileName Name of the output graph file.
      */
-    public static void writeGraph(Graph graph, String outputFileName) {
-        FileSink fileSink = new FileSinkDOT(true);
-
+    public void writeGraph(Graph graph, String outputFileName) {
         try {
-            fileSink.writeAll(graph, outputFileName);
-        } catch (IOException e) {
+            PrintWriter graphWriter = new PrintWriter(outputFileName);
+
+            // Header
+            graphWriter.println("digraph " + _graphID + " {");
+
+            // Print Nodes.
+            graph.nodes().forEach(node -> {
+                Double weight = (Double) node.getAttribute("Weight");
+                graphWriter.println(new StringBuilder()
+                        .append(node.getId())
+                        .append(" [Weight=")
+                        .append(weight.intValue())
+                        .append(",Start=")
+                        .append((int)node.getAttribute("Start"))
+                        .append(",Processor=")
+                        .append((int)node.getAttribute("Processor"))
+                        .append("];")
+                );
+            });
+
+            // Print edges
+            graph.edges().forEach(edge -> {
+                Double weight = (Double)edge.getAttribute("Weight");
+                graphWriter.println(new StringBuilder()
+                        .append(edge.getSourceNode().getId())
+                        .append(" -> ")
+                        .append(edge.getTargetNode().getId())
+                        .append(" [Weight=")
+                        .append(weight.intValue())
+                        .append("];")
+                );
+            });
+
+            graphWriter.println("}");
+            graphWriter.close();
+        } catch (FileNotFoundException e) {
+            // Here, since all we are doing is creating a new file to write it, this error is not expected to happen.
             e.printStackTrace();
         }
     }
