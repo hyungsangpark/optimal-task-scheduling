880cbf41de483f7009577fc14c06259521ee5ce9
DanilLinkovSylo <danil@sylo.io>
2021-08-18 16:18:05 +1200

    adding a main in valid tester class

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,19 +1,5 @@
 import java.io.File;
 
 public class Main {
-    public static void main(String[] args) {
-        ValidOutputTester solutionValidator = new ValidOutputTester();
 
-        File[] files = new File("./src/test/graphs/").listFiles();
-
-        for (File file : files) {
-            if (file.isFile()) {
-                String input = file.getPath();
-                String output = file.getPath()
-                        .replace("graphs", "actualOutputs")
-                        .replace(".dot", "-output.dot");
-                solutionValidator.isValid(input,output, 2);
-            }
-        }
-    }
 }
--- src/test/java/ValidOutputTester.java
+++ src/test/java/ValidOutputTester.java
@@ -3,11 +3,28 @@ import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import project1.util.GraphLoader;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ValidOutputTester {
+
+    public static void main(String[] args) {
+        ValidOutputTester solutionValidator = new ValidOutputTester();
+
+        File[] files = new File("./src/test/graphs/").listFiles();
+
+        for (File file : files) {
+            if (file.isFile()) {
+                String input = file.getPath();
+                String output = file.getPath()
+                        .replace("graphs", "actualOutputs")
+                        .replace(".dot", "-output.dot");
+                solutionValidator.isValid(input,output, 2);
+            }
+        }
+    }
 
     public boolean isValid(String inputFile, String outputFile, int numOfProcessors) {
         GraphLoader graphLoader = new GraphLoader();
