9ca7ccec72380bf17affd1575bcd99f6497f7251
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-08-20 00:37:32 +1200

    Refactored Visualiser class by removing unnecessary or redundant codes.

--- src/main/java/project1/gui/Visualiser.java
+++ src/main/java/project1/gui/Visualiser.java
@@ -2,7 +2,6 @@ package project1.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,33 +9,22 @@ import javafx.stage.Stage;
  * The Visualiser Class is responsible for loading the GUI.
  * Will add more comments
  */
-
 public class Visualiser extends Application {
 
-    private Controller controller;
-    Scene scheduleScene;
-    public Stage program;
-    @Override
-    public void start(Stage program) throws Exception{
-        FXMLLoader loader = new FXMLLoader((getClass().getResource("Visualiser.fxml")));
-        Parent root= loader.load();
-        this.controller= loader.getController();
+    // Main class needed to launch JavaFX Application.
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-        scheduleScene= new Scene(root, 400, 600);
-        program.setScene(scheduleScene);
-        program.setTitle("Schedule Visualiser");
+    @Override
+    public void start(Stage program) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Visualiser.fxml"));
+
+        program.setScene(new Scene(loader.load(), 400, 600));
+        program.setTitle("Task Scheduler Visualiser");
         program.show();
 
         // Closing the GUI and algorithm.
         program.setOnCloseRequest(event -> System.exit(0));
-
-
-    }
-    /**
-     * This method sets up the fields for the Controller class that are to \
-     * be displayed on the GUI.
-     */
-    public void ArgsSetup(int numProcessors, int numTasks, String graphName){
-        controller.ArgsSetup(numProcessors, numTasks, graphName);
     }
 }
