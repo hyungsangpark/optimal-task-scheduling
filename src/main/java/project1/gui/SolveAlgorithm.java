ffe864dfa881222947ed0705fd19d314f00082fc
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-08-21 23:57:34 +1200

    Removed deprecated methods used for testing which were commented out.

--- src/main/java/project1/gui/SolveAlgorithm.java
+++ src/main/java/project1/gui/SolveAlgorithm.java
@@ -24,47 +24,7 @@ public class SolveAlgorithm extends Thread {
         optimalTime = schedule.getOptimalTime();
 
         isFinished = true;
-
-        // To be replaced with a proper algorithm runner.
-//        methodForTesting();
     }
-
-//    // Remove this method as it is just for testing purposes.
-//    private void methodForTesting() {
-//        try {
-//            sleep(1340);
-//            schedule = new NewScheduleNode[10];
-//            schedule[0] = new NewScheduleNode("A", 0, 3, 1);
-//            schedule[1] = new NewScheduleNode("B", 2, 3, 2);
-//            schedule[2] = new NewScheduleNode("C", 3, 5, 1);
-//            schedule[3] = new NewScheduleNode("D", 4, 6, 3);
-//            schedule[4] = new NewScheduleNode("E", 4, 6, 2);
-//            schedule[5] = new NewScheduleNode("F", 1, 6, 4);
-//            schedule[6] = new NewScheduleNode("G", 4, 6, 5);
-//            schedule[7] = new NewScheduleNode("H", 4, 6, 6);
-//            schedule[8] = new NewScheduleNode("I", 4, 6, 7);
-//            schedule[9] = new NewScheduleNode("J", 4, 6, 8);
-//            isChanged = true;
-//            sleep(1365);
-//            schedule[0] = new NewScheduleNode("K", 0, 3, 6);
-//            schedule[1] = new NewScheduleNode("L", 2, 3, 4);
-//            schedule[3] = new NewScheduleNode("N", 4, 6, 6);
-//            schedule[4] = new NewScheduleNode("O", 4, 6, 1);
-////            schedule[5] = new NewScheduleNode("P", 1, 6, 7);
-//            schedule[6] = new NewScheduleNode("Q", 1, 2, 3);
-//            schedule[7] = new NewScheduleNode("R", 2, 5, 2);
-//            schedule[8] = new NewScheduleNode("S", 4, 5, 3);
-//            schedule[9] = new NewScheduleNode("T", 1, 5, 5);
-//            isChanged = true;
-//            sleep(884);
-//            schedule[2] = new NewScheduleNode("M", 3, 5, 8);
-//            isChanged = true;
-//            optimalTime = 6;
-//            isFinished = true;
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     public void changeApplied() {
         isChanged = false;
