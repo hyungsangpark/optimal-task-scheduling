0b33f296500fa5d0e435bec24d0a66912b71db84
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-08-21 23:42:46 +1200

    Fixed Main class to properly process CLI version of scheduler.

--- src/main/java/project1/main/Main.java
+++ src/main/java/project1/main/Main.java
@@ -1,18 +1,40 @@
 package project1.main;
 
 import org.apache.commons.cli.*;
-import org.graphstream.graph.Graph;
-import project1.algorithm.Astar;
+import project1.IO.GraphReader;
+import project1.IO.GraphWriter;
+import project1.algorithm.AStar;
+import project1.algorithm.TotalFCostCalculator;
 import project1.data.ScheduleNode;
-import project1.util.GraphLoader;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+
+//        File[] files = new File("./src/test/graphs/").listFiles();
+//
+//        for (File file : files) {
+//            if (file.isFile()) {
+//                args = new String[2];
+//                args[0] = file.getPath();
+//                args[1] = "2";
+//
+//                run(args);
+//                System.out.println("Finished file: " + file.getName());
+//            }
+//            GraphReader.getInstance().resetGraphReader();
+//        }
+
+        args = new String[2];
+        args[0] = "./src/test/graphs/sample.dot";
+        args[1] = "2";
+        run(args);
+    }
+
+    public static void run(String[] args) {
 
         // Do a simple check on whether at least two arguments have been inputted.
         if (args == null || args.length < 2) {
@@ -88,31 +110,31 @@ public class Main {
 
         // Read graph, run algorithm, then write graph.
         try {
-            GraphLoader graphLoader = new GraphLoader();
+            GraphReader graphReader = GraphReader.getInstance();
+            graphReader.loadGraphData(graphFileName);
 
-            Graph graph = graphLoader.readGraph(graphFileName);
+            // Only for testing
+            TotalFCostCalculator.getInstance().resetGraphReader();
 
             // Record the start time.
             long startTime = System.nanoTime();
 
             // Run ALGORITHM to receive schedule.
-            Astar newSearch = new Astar(graph, parameters.getNumProcessors());
+            AStar newSearch = new AStar(parameters.getNumProcessors(), parameters.getNumParallelCores());
             ScheduleNode result = newSearch.aStarSearch();
-            graphLoader.formatOutputGraph(graph, result.getSchedule());
 
             // Record the end time.
             long endTime = System.nanoTime();
             long duration = (endTime - startTime)/1000;
 
-            graphLoader.writeGraph(graph, parameters.getOutputName());
+            GraphWriter graphWriter = new GraphWriter();
+            graphWriter.outputGraphData(parameters.getOutputName().replace("graphs","actualOutputs"),result.getScheduleMap());
 
             // Output results.
             System.out.println("\nOutput written to file named: " + parameters.getOutputName());
             System.out.println("Time taken: " + duration + " ms");
+            System.out.println("Finish time of best schedule: " + result.getOptimalTime());
 
-            // Find out the finish time.
-            result.getSchedule().sort(Comparator.comparingInt(List::size));
-            System.out.println("Finish time of best schedule: " + result.getSchedule().get(result.getSchedule().size() - 1).size());
         } catch (IOException e) {
             System.err.println("ERROR: Graph file with the provided name is not found.");
             System.exit(1);
